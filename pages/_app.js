5551980e7679f4114ef40fb63d5e141ad3163878
Claire Wang <clara32356@gmail.com>
2023-04-27 11:51:55 -0400

    add unfurl and favicone

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import '@/styles/globals.scss'
 import Head from 'next/head'
+import Meta from '../components/meta'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <Meta />
         <script
           defer
           data-domain="angelhacks.org"
           src="https://plausible.io/js/script.js"
         />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <title>AngelHacks 3.0 | A game jam for the ages!</title>
       </Head>
       <Component {...pageProps} />
