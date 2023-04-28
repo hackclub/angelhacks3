0bc5fb98877b6b9b8e1e9b4356d7073b561624c4
Claire Wang <clara32356@gmail.com>
2023-04-27 21:13:06 -0400

    add los angeles satellite link

--- components/Satellites.js
+++ components/Satellites.js
@@ -3,7 +3,7 @@ import styles from './Satellites.module.scss'
 export default function Satellites() {
   return (
     <div className={styles.wrapper}>
-      Can't make it to Boston? Try <a href="#">Los Angeles</a> /{' '}
+      Can't make it to Boston? Try <a href="https://angelhacks-la.hackclub.dev/">Los Angeles</a> /{' '}
       <a href="https://angelhacksba.hackclub.com/">Bay Area</a> / <a href="https://angelhacksto.hackclub.com/">Toronto</a>
     </div>
   )
