53516acc01b81cf57f1d0603adffb3ea5f8fb2f5
Claire Wang <clara32356@gmail.com>
2023-05-02 13:28:57 -0400

    add hackclub sbumdomain tracking too

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }) {
           data-domain="angelhacks.org"
           src="https://plausible.io/js/script.js"
         />
+        <script defer data-domain="angelhacks.hackclub.com" src="https://plausible.io/js/script.js"></script>
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
